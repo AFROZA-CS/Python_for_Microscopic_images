e8c0743418e70ebb821e48e053b275397767f3c8
Afroza <143857942+AFROZA-CS@users.noreply.github.com>
2025-07-02 14:09:28 +0100

    Add files via upload

---
+++ logical_operators_python.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+-------------------------------------------
+a =3
+b=5.0
+c=a/b
+
+if a >= 5:
+    print("hello world")
+else:
+    print("my name is afroza")
+--------------------------------------------
+
+import math
+
+a =9
+b= math.sqrt(a)
+
+print(b)
+----------------------------------------------------
+Equaltion for this python file
+F = ma
+E = mc**2
+
+enter mass in kilogram
+"""
+
+# mass = 5 # in kilograms
+
+mass = float(input("please enter the mass: "))
+c = 30000000 # meters per second
+
+a= 9 #  meters per second sqaure
+
+Force = mass* a
+
+
+# Energy = mass * (c**2)
+
+print("Force =", Force)
+# print("Energy=", Energy)
+
+
+
+
